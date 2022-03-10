73a796d2de81e002810430a2cc1a4524201d39a0
RawRaw5 <94robertwhite@gmail.com>
2022-03-09 22:03:12 -0800

    create and new route complete

--- views/index.jsx
+++ views/index.jsx
@@ -4,20 +4,21 @@ const Default = require('./layouts/Default')
 function Index (props) {
     const breads = props.breads;
     return (
-      <Default title={props.title}>
-          <ul>
-          {breads.map((bread, index) => {
+        <Default title={props.title}>
+            <ul>
+            {breads.map((bread, index) => {
               return (
                   <li key={index}>
                       <a href={`/breads/${index}`}>
                           {bread.name}
                       </a>
                   </li>
-              )
-          })}
-          </ul>
-      </Default>
+                )
+            })}
+            </ul>
+        </Default>
     )
 }
 
 module.exports = Index
+
