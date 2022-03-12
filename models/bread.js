9c1cb6a332d7067317f038a2a41e06cc9bfc1ca1
RawRaw5 <94robertwhite@gmail.com>
2022-03-12 14:12:14 -0800

    edit and update routes complete

--- models/bread.js
+++ models/bread.js
@@ -3,20 +3,35 @@ module.exports = [
       name: 'Rye',
       hasGluten: true,
       image: 'https://images.unsplash.com/photo-1595535873420-a599195b3f4a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+      ingredients: ['yeast', 'water', 'gluten-free flour', 'baking powder', 'salt'],
+      directions: ['mix all the ingredients','turn oven to 325 degrees', 'bake for 30 minutes'],
     },
     {
       name: 'French',
       hasGluten: true,
        image: 'https://images.unsplash.com/photo-1534620808146-d33bb39128b2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
+       ingredients: ['yeast', 'water', 'gluten-free flour', 'baking powder', 'salt'],
+       directions: ['mix all the ingredients','turn oven to 325 degrees', 'bake for 30 minutes'],
     },
     {
       name: 'Gluten-Free',
       hasGluten: false,
       image: 'https://images.unsplash.com/photo-1546538490-0fe0a8eba4e6?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80',
+      ingredients: ['yeast', 'water', 'gluten-free flour', 'baking powder', 'salt'],
+      directions: ['mix all the ingredients','turn oven to 325 degrees', 'bake for 30 minutes'],
     },
     {
       name: 'Pumpernickel',
       hasGluten: true,
       image: 'https://images.unsplash.com/photo-1586444248902-2f64eddc13df?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80',
+      ingredients: ['yeast', 'water', 'gluten-free flour', 'baking powder', 'salt'],
+      directions: ['mix all the ingredients','turn oven to 325 degrees', 'bake for 30 minutes'],
+    },
+    {
+      name: 'Banana Bread',
+      hasGluten: true,
+      image:'https://rasamalaysia.com/wp-content/uploads/2019/09/best-banana-bread-thumb.jpg',
+      ingredients: ['yeast', 'water', 'gluten-free flour', 'baking powder', 'salt'],
+      directions: ['mix all the ingredients','turn oven to 325 degrees', 'bake for 30 minutes'],
     }
   ]
--- views/show.jsx
+++ views/show.jsx
@@ -4,9 +4,11 @@ const Default = require('./layouts/Default')
 function Show ({bread, index}) {
     // Confirm we are getting our bread data in the terminal.
     console.log(bread)
+    console.log(index)
       return (
         <Default>
         <h3>{bread.name}</h3>
+        <a href={`/breads/${index}/edit`}><button>Edit</button></a>
         <form action={`/breads/${index}?_method=DELETE`} method="POST">
           <input type='submit' value="DELETE"/>
         </form>
@@ -19,6 +21,13 @@ function Show ({bread, index}) {
           }
           have gluten.
         </p>
+        <h4>Ingridents</h4>
+        <ul>
+        {bread.ingridents.map(ingrident =><li key={index}>{ingridents}</li>)}
+        </ul>
+        <ol>
+        {bread.directions.map(direction =><li key={index}>{directions}</li>)}
+        </ol>
         <img src={bread.image} alt={bread.name} />
         <li><a href="/breads">Go home</a></li>
          </Default>
